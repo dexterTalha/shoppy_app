2187655acdad1ffcbf3c0d1f307fd7f699c38ef9
Mohammad Talha <talhaviet22@gmail.com>
2020-03-06 18:07:45 +0530

    My orders page

--- lib/screens/my_orders.dart
+++ lib/screens/my_orders.dart
@@ -1,3 +1,4 @@
+import 'package:shoppy_app/util/data.dart';
 import 'package:flutter_icons/feather.dart';
 import 'package:shoppy_app/widgets/badge.dart';
 import 'package:flutter/material.dart';
@@ -8,6 +9,14 @@ class MyOrders extends StatefulWidget {
 }
 
 class _MyOrdersState extends State<MyOrders> {
+  bool isExpanded = false;
+
+  _expand() {
+    setState(() {
+      isExpanded = !isExpanded;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,6 +36,99 @@ class _MyOrdersState extends State<MyOrders> {
             ),
           ),
         ],
+      ),
+      body: Container(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 10.0, right: 20, left: 20),
+          child: ListView.builder(
+            scrollDirection: Axis.vertical,
+              shrinkWrap: true,
+              itemCount: furnitures.length,
+              itemBuilder: (context, index) {
+                Map furniture = furnitures[index];
+                return Padding(
+                  padding: const EdgeInsets.only(top: 15.0),
+                  child: generateMyOrders(furniture, index),
+                );
+              }),
+        ),
+      ),
+    );
+  }
+
+  Widget generateMyOrders(furniture, int index) {
+    return GestureDetector(
+      onTap: _expand,
+      child: Material(
+        elevation: 6,
+        borderRadius: BorderRadius.circular(10),
+        child: AnimatedContainer(
+          duration: Duration(milliseconds: 600),
+          height: isExpanded ? MediaQuery.of(context).size.width / 2 : 100,
+          width: double.infinity,
+          child: Row(
+            children: <Widget>[
+              ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.asset(
+                  '${furniture["img"]}',
+                  height: 100,
+                  width: 100,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.only(top: 5.0, left: 12.0, bottom: 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      "${furniture["name"]}",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w800,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Flexible(
+                      child: Text(
+                        "Description lorem ipsum",
+                        style: TextStyle(
+                          fontSize: 14,
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1,
+                        softWrap: true,
+                        textAlign: TextAlign.start,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Flexible(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(
+                            "\$500",
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w800,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
